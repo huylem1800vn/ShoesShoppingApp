a924837836dc34887b7e3b25e747a1d34f9c7ff7
@daquynh2403 <daquynh2403@gmail.com>
2022-05-05 01:14:22 +0700

    Initial commit

---
+++ app/src/main/java/com/ldq/ltdd_cs92_nhom6_shoesshoppingapp/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.ldq.ltdd_cs92_nhom6_shoesshoppingapp.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
